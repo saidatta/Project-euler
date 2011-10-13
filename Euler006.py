55caa5bd390e99144ee98c223cd4f50587a4ffa3
Venkata Munnangi <venkatamunnangi@gmail.com>
2011-10-13 12:16:37 -0700

    Some solutions

---
+++ Euler006.py
@@ -0,0 +1,10 @@
+import math
+
+i = 0
+sum = 0
+while i < 101:
+  sum += math.pow(i,2) 
+  i += 1
+  
+  y = sum - math.pow(5050, 2)
+ print "%d"%y
---
+++ Euler099.py
@@ -0,0 +1,13 @@
+from math import log
+
+mv =0
+ml = 0
+ln = 0
+
+for e_line in file('base_exp.txt'):
+	ln+=1
+	base,exp = e_line.split(',')
+	value = int(exp)*log(int(base))
+	if value > mv:
+		mv,ml = value,ln
+print ml
